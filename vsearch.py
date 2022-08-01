164910c2ecb2678dacf61da8a674905960abefe9
aleklandra <aleklandra@yandex.ru>
2022-08-02 00:03:46 +0300

    it's a module for webapp

---
+++ vsearch.py
@@ -0,0 +1,8 @@
+def search4vowels(phrase:str) -> set:
+    """Выводит гласные, найденные во введенном слове."""
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str, letters:str) -> set:
+    """Возвращает множество букв из 'letters', найденных в указанной фразе"""
+    return set(letters).intersection(set(phrase))
